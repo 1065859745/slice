12a71f4b7298e0a9e72f9cad79aa72247c1759b2
unknown <1065859745@qq.com>
2020-09-24 11:48:26 +0800

    Update multiple parameter

--- update.go
+++ update.go
@@ -1,13 +1,13 @@
 package slice
 
 // UpdateStr 插入新元素，若切片中已存在，则忽略.
-func UpdateStr(arr []string, s string) []string {
-	if arr != nil {
-		for i := 0; i < len(arr)-1; i++ {
-			if arr[i] == s {
+func UpdateStr(arr []string, s ...string) []string {
+	for i := 0; i < len(arr)-1; i++ {
+		for _, v := range s {
+			if arr[i] == v {
 				return arr
 			}
 		}
 	}
-	return append(arr, s)
+	return append(arr, s...)
 }
